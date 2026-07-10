84573caba03e24565e0e2ed56d3a3c277003a4b2
thakurgiridhar862-bit <thakurgiridhar862@gmail.com>
2026-07-10 16:22:04 +0530

    Add inverted right angled star triangle pattern

---
+++ patterns/pattern5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+void pattern5(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = n; j >= i; j--)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int x;
+    cout << "Enter the number of rows : ";
+    cin >> x;
+    pattern5(x);
+    return 0;
+}
