444b18bf09d5c40d03e43c452a75146375a8a1fc
thakurgiridhar862-bit <thakurgiridhar862@gmail.com>
2026-07-10 16:28:47 +0530

    Add diamond star pattern

---
+++ patterns/pattern9.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+void pattern9(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+    for (int i = n; i > 0; i--)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < 2 * i - 1; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int x;
+    cout << "Enter the number of rows : ";
+    cin >> x;
+    pattern9(x);
+    return 0;
+}
